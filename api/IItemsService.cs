25d21d1e5bf7a47b9e2e9c08ffdd0bfab1c6f596
chantal <chanty98@hotmail.com>
2019-12-14 19:28:08 +0200

    implement items service

--- api/IItemsService.cs
+++ api/IItemsService.cs
@@ -1,9 +1,9 @@
-﻿using Shopify.Models;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ServiceModel;
 using System.ServiceModel.Web;
+using Shopify.Models;
 
-namespace Shopify.Services
+namespace Shopify.Api
 {
     [ServiceContract]
     public interface IItemsService
@@ -12,9 +12,45 @@ namespace Shopify.Services
         [WebInvoke(
             Method = "GET",
             ResponseFormat = WebMessageFormat.Json,
-            BodyStyle = WebMessageBodyStyle.Wrapped,
-            UriTemplate = "GetAll"
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/"
         )]
         List<Item> GetAll();
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/{id}"
+        )]
+        Item GetItem(string id);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/"
+        )]
+        Item CreateItem(Item item);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "PATCH",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/{id}"
+        )]
+        Item UpdateItem(string id, Item item);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "DELETE",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/{id}"
+        )]
+        void DeleteItem(string id);
     }
 }
--- api/ItemsService.svc.cs
+++ api/ItemsService.svc.cs
@@ -2,19 +2,46 @@
 using System.Linq;
 using Shopify.Models;
 using Shopify.Db;
-using System.ServiceModel.Activation;
 
-namespace Shopify.Services
-{ 
-    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+namespace Shopify.Api
+{
     public class ItemsService : IItemsService
     {
         private ShopifyContext _dbContext = new ShopifyContext();
+
+        public Item CreateItem(Item item)
+        {
+            var created = this._dbContext.Items.Add(item);
+            this._dbContext.SaveChanges();
+            return created;
+        }
+
+        public void DeleteItem(string id)
+        {
+            var item = this._dbContext.Items.Find(new object[] { int.Parse(id) });
+            this._dbContext.Items.Remove(item);
+            this._dbContext.SaveChanges();
+        }
 
         public List<Item> GetAll()
         {
             var items = from item in this._dbContext.Items select item;
             return items.ToList();
+        }
+
+        public Item GetItem(string id)
+        {
+            return this._dbContext.Items.Find(new object[] { int.Parse(id) });
+        }
+
+        public Item UpdateItem(string id, Item item)
+        {
+            var oldItem = this._dbContext.Items.Find(new object[] { int.Parse(id) });
+            oldItem.Name = item.Name;
+            oldItem.Price = item.Price;
+            oldItem.Description = item.Description;
+            this._dbContext.SaveChanges();
+            return oldItem;
         }
     }
 }
