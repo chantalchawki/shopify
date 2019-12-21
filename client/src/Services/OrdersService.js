58a5ad01113a49728536a092065bf7e582a83037
karimelsayed98 <karim.abdelghaffar98@gmail.com>
2019-12-21 17:20:34 +0200

    Added services

---
+++ client/src/Services/OrdersService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+function getAllOrders() {
+  return axios.get("http://localhost:62248/api/OrdersService.svc/");
+}
+
+function getOrder(orderId) {
+  return axios.get(`http://localhost:62248/api/OrdersService.svc/${orderId}`);
+}
+
+function createOrder(order) {
+  return axios.post("http://localhost:62248/api/OrdersService.svc/", { order });
+}
+export default {
+  getAllOrders,
+  getOrder,
+  createOrder
+};
