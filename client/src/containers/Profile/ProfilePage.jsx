264b8362087afeb33e13c837543f55d991ea0bd7
mariamkamel <mariammkamel98@gmail.com>
2019-12-21 18:58:50 +0200

    header/mobile input added

--- client/src/containers/Profile/ProfilePage.jsx
+++ client/src/containers/Profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
 import {
   Grid,
   Button,
@@ -15,12 +15,11 @@ import styles from "./styles";
 
 function ProfilePage({ classes }) {
   const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [area, setArea] = useState("");
   const [address, setAddress] = useState("");
-
+  const [mobile, setMobile] = useState("");
   const handleSubmit = (event, props) => {
     event.preventDefault();
   };
@@ -35,109 +34,115 @@ function ProfilePage({ classes }) {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <PersonIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Profile
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-                value={profile.name}
-                onChange={e => setFName(e.target.value)}
-              />
+    <Grid container className={classes.root}>
+      <Grid container item className={classes.header}>
+        <Header />
+      </Grid>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <PersonIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Profile
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  value={profile.name}
+                  onChange={e => setFName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={profile.mail}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  value={profile.password}
+                  autoComplete="current-password"
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="address"
+                  label="Address"
+                  id="address"
+                  autoComplete="address"
+                  value={profile.address}
+                  onChange={e => setAddress(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="mobile"
+                  label="Mobile"
+                  id="mobile"
+                  autoComplete="mobile"
+                  value={profile.mobile}
+                  onChange={e => setMobile(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="area"
+                  label="Area"
+                  id="area"
+                  autoComplete="area"
+                  value={profile.area}
+                  onChange={e => setArea(e.target.value)}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-                onChange={e => setLName(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                value={profile.mail}
-                onChange={e => setEmail(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                value={profile.password}
-                autoComplete="current-password"
-                onChange={e => setPassword(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="address"
-                label="Address"
-                id="address"
-                autoComplete="address"
-                value={profile.address}
-                onChange={e => setAddress(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="area"
-                label="Area"
-                id="area"
-                autoComplete="area"
-                value={profile.area}
-                onChange={e => setArea(e.target.value)}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Edit
-          </Button>
-        </form>
-      </div>
-    </Container>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Edit
+            </Button>
+          </form>
+        </div>
+      </Container>
+    </Grid>
   );
 }
 
--- client/src/containers/Profile/styles.js
+++ client/src/containers/Profile/styles.js
@@ -9,6 +9,9 @@ export default theme => ({
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main
   },
+  header: {
+    flexDirection: "row"
+  },
   form: {
     width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3)
