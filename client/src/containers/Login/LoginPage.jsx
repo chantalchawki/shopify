a2c7ee5fb63c511c20a888dca3f945fddb7b5cc5
Khaled Mohamed <khal3d.mohamed@gmail.com>
2019-12-22 02:11:49 +0200

    WIP: login with google

--- client/src/containers/Login/LoginPage.jsx
+++ client/src/containers/Login/LoginPage.jsx
@@ -26,8 +26,31 @@ function LoginPage({ classes }) {
     let res = UsersService.login(user);
     localStorage.setItem("token", res.data.token);
   };
+
+  const googleLogin = async () => {
+    window.gapi.load("auth2", () => {
+      window.gapi.auth2
+        .init({
+          client_id: "126151922298-jv8d5mev97sho4qb7ns97qt3hmibampb.apps.googleusercontent.com",
+        })
+        .then(() => {
+          window.gapi.signin2.render("my-signIn", {
+            scope: "profile email",
+            width: 250,
+            height: 50,
+            longtitle: false,
+            theme: "dark",
+            onsuccess: console.log,
+            onfailure: console.log
+          });
+        });
+    });
+  };
+
   return (
     <Container component="main" maxWidth="xs">
+      <div id="my-signIn" />
+      <button onClick={googleLogin}>Google</button>
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
